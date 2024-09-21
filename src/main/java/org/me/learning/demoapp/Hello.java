ccd8f0ab7462a07fc0024e0a01b0da6259d339ac
mohamadalabsi <absimoda1@gmail.com>
2024-09-21 13:51:24 +0200

    Spring First steps

---
+++ src/main/java/org/me/learning/demoapp/Hello.java
@@ -0,0 +1,12 @@
+package org.me.learning.demoapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @RequestMapping("/")
+    public  String greeting (){
+         return  "Hello World";
+    }
+}
